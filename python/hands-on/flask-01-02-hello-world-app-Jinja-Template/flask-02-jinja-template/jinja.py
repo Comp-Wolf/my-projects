20bd08afc91241c574aaddedc327d0d9e408996e
Comp-Wolf <alierdogan38@gmail.com>
2022-01-19 00:08:25 +0300

    flask-01 and flask-02 added

--- python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-02-jinja-template/jinja.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def head():
-    return render_template("index.html", number1 = 7000, number2 = 9000)
+    return render_template('index.html', number1 = 7000, number2 = 9000)
 
-@app.route("/")
-def head():
-    var1, var2 = 3040, 7060
-    return render_template("body.html", num1 = var1, num2 = var2, multiplication = var1*var2)
+@app.route('/mult')
+def number():
+    var1, var2 = 1071, 1453
+    return render_template('body.html', num1 = var1, num2 = var2, multiplication = var1*var2)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
