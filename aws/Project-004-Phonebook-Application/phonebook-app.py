4c18c32b41b668f62c0d3426ddcc76a791e21ad3
Comp-Wolf <alierdogan38@gmail.com>
2022-03-02 15:12:46 +0300

    new proje

--- aws/Project-004-Phonebook-Application/phonebook-app.py
+++ aws/Project-004-Phonebook-Application/phonebook-app.py
@@ -12,7 +12,7 @@ db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8')
 
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Pl123456789'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Ali_123456'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway_phonebook'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
